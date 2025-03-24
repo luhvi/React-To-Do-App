0ad25c6dd65131b6d49a35813684fb2e12e20a69
luhvi <luhvilive@gmail.com>
2025-03-24 13:55:17 +0100

    second commit

---
+++ src/Header.jsx
@@ -0,0 +1,7 @@
+const Header = () => {
+  return (
+    <header>
+      <p>to do list</p>
+    </header>
+  );
+};
--- src/SubmitTask.tsx
+++ src/SubmitTask.tsx
@@ -1,14 +1,20 @@
+import { JSX } from 'react';
 import '@fortawesome/fontawesome-free/css/all.min.css';
 
-const SubmitTask = () => {
+const SubmitTask = (): JSX.Element => {
+  const handleSubmit = (formData: FormData): void => {
+    const task: FormDataEntryValue | null = formData.get('task');
+    console.log(task);
+  };
+
   return (
     <div className="flex items-center justify-center">
-      <button className="text-sky-400 bg-[#262626] mr-4 pt-3 pb-3 pl-4 pr-4 rounded-full drop-shadow-md shadow-md cursor-pointer">
-        <i className="fa-solid fa-magnifying-glass"></i>
-      </button>
-      <form action="">
+      <form action={handleSubmit}>
+        <button className="text-white bg-dark-gray mr-4 pt-3 pb-3 pl-4 pr-4 rounded-full drop-shadow-md shadow-md cursor-pointer">
+          <i className="fa-solid fa-magnifying-glass"></i>
+        </button>
         <input
-          className="text-white placeholder:text-white bg-[#262626] focus:outline-hidden pt-3 pb-3 pl-4 pr-4 rounded-sm drop-shadow-md shadow-md"
+          className="text-white placeholder:text-white bg-dark-gray focus:outline-hidden pt-3 pb-3 pl-4 pr-4 rounded-sm drop-shadow-md shadow-md"
           type="text"
           placeholder="E.g. Workout"
           name="task"
